82c649b299cf85c315972588c38b9c7acceadd98
skanner2 <63942471+skanner2@users.noreply.github.com>
2022-08-24 09:36:53 -0400

    initial commit

--- test/src/main/java/com/example/test/TestApplication.java
+++ test/src/main/java/com/example/test/TestApplication.java
@@ -2,12 +2,17 @@ package com.example.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@RestController
 public class TestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(TestApplication.class, args);
+  }
 
 }
+
